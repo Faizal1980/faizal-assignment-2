590fcdaa33fe85494340626fd2371f05470770cb
Faizal1980 <149090943+Faizal1980@users.noreply.github.com>
2024-01-30 16:55:38 -0500

    Update Assignment2Application.java

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -6,32 +6,38 @@ import java.util.Scanner;
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-
 		Random random = new Random();
 		int theRandomNumber = random.nextInt(1, 101);
-		
-		for (int i = 1; i < 6; i++) {
+		Scanner scanner = new Scanner(System.in);
 
-			Scanner scanner = new Scanner(System.in);
-			System.out.println("Pick a number between 1 and 100");
+		int i = 0;
+		while (i < 5) {
+			System.out.print("Pick a number between 1 and 100 ");
 			String userInput = scanner.nextLine();
 			Integer convertedInput = Integer.parseInt(userInput);
 
-			if (convertedInput < 1 | convertedInput > 100) {
-				System.out.println("Your guess is not between 1 and 100, please try again");
-			} else if (theRandomNumber == convertedInput) {
+			if (convertedInput < 1 || convertedInput > 100) {
+				System.out.println("The number was not between 1 and 100, please try again");
+				continue;
+			}
+			i++;
+
+			if (convertedInput == theRandomNumber) {
 				System.out.println("You win!");
-				System.exit(0);
+				break;
 			} else if (convertedInput < theRandomNumber) {
 				System.out.println("Please pick a higher number");
-			} else if (convertedInput > theRandomNumber) {
+			} else {
 				System.out.println("Please pick a lower number");
 			}
-
 		}
 
-		System.out.println("You lose!");
-		System.out.println("The number to guess was: " + theRandomNumber);
+		if (i == 5) {
+			System.out.println("You lose!");
+			System.out.println("The number to pick was: " + theRandomNumber);
+		}
 
+		scanner.close();
 	}
+
 }
